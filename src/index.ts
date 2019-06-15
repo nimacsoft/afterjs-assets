4058d53b715038488d16cdc6b3688f20e9d13a46
nima <nimaarefi@outlook.com>
2019-06-16 02:59:13 +0430

    fix some bugs before release

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,42 @@
-import { matchPath } from "react-router-dom"
-import { getAssets } from "./types"
+import { matchPath } from 'react-router-dom';
+import { getAssets } from './types';
 
 function getAssests({ req, routes, manifest }: getAssets) {
-	let scripts: string[] = []
-	let styles: string[] = []
-	const match = routes.find((route) => {
-		return !!matchPath(req.url, route)
-	})
+  let scripts: string[] = [];
+  let styles: string[] = [];
+  const match = routes.find(route => {
+    return !!matchPath(req.url, route);
+  });
 
-	if (match) {
-		const route = routes.find((item) => item.path === match.path)
+  if (match) {
+    const route = routes.find(item => item.path === match.path);
 
-		if (!route) return { scripts, styles }
+    if (!route) return { scripts, styles };
 
-		if (typeof route.name === "undefined" && process.env.NODE_ENV !== "production") {
-			throw new Error(
-				`routes must have a "name" key with value of chunk name ${JSON.stringify(
-					{ name: "ChunkName", ...route },
-					null,
-					2
-				)}`
-			)
-		}
-		if (manifest[name] && manifest[name].js) {
-			scripts = manifest[name].js
-		}
+    if (!route.name && process.env.NODE_ENV !== 'production') {
+      throw new Error(
+        `routes must have a "name" key with value of chunk name ${JSON.stringify(
+          { name: 'ChunkName', ...route },
+          null,
+          2
+        )}`
+      );
+    }
 
-		if (manifest[name] && manifest[name].css) {
-			styles = manifest[name].css
-		}
-	}
+    if (route.name) {
+      const { name: chunkName } = route;
 
-	return { scripts, styles }
+      if (manifest[chunkName] && manifest[chunkName].js) {
+        scripts = manifest[chunkName].js;
+      }
+
+      if (manifest[chunkName] && manifest[chunkName].css) {
+        styles = manifest[chunkName].css;
+      }
+    }
+  }
+
+  return { scripts, styles };
 }
 
-export default getAssests
+export default { getAssests };
